567711e9d152e4121d9752b85afb42c99c0269cb
iserge <s.serge.b@gmail.com>
2016-11-28 13:25:08 +0300

    All Cesium functions moved to Cesium.java

--- gwt-cs-test/src/main/java/org/cleanlogic/showcase/client/examples/Camera.java
+++ gwt-cs-test/src/main/java/org/cleanlogic/showcase/client/examples/Camera.java
@@ -21,6 +21,7 @@ import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.user.client.ui.*;
+import org.cesiumjs.cs.Cesium;
 import org.cesiumjs.cs.Configuration;
 import org.cesiumjs.cs.core.*;
 import org.cesiumjs.cs.core.Math;
