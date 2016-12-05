ac5040f6426ae5c44514d258948544608a430279
iserge <s.serge.b@gmail.com>
2016-12-05 10:56:55 +0300

    New geometry - RectangleGeometry

---
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/core/geometry/RectangleGeometry.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2016 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.core.geometry;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import org.cesiumjs.cs.core.Packable;
+import org.cesiumjs.cs.core.geometry.options.RectangleGeometryOptions;
+import org.cesiumjs.cs.scene.Primitive;
+
+/**
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = "Cesium", name = "RectangleGeometry")
+public class RectangleGeometry implements Packable {
+    /**
+     * The number of elements used to pack the object into an array.
+     */
+    @JsProperty
+    public static int packedLength;
+
+    /**
+     * A description of a cartographic rectangle on an ellipsoid centered at the origin.
+     * Rectangle geometry can be rendered with both {@link Primitive} and {@link GroundPrimitive}.
+     * @param options {@link RectangleGeometryOptions}
+     */
+    @JsConstructor
+    public RectangleGeometry(RectangleGeometryOptions options) {}
+
+    /**
+     * Computes the geometric representation of an rectangle, including its vertices, indices, and a bounding sphere.
+     * @param rectangleGeometry A description of the rectangle.
+     * @return The computed vertices and indices.
+     */
+    @JsMethod
+    public static native Geometry createGeometry(RectangleGeometry rectangleGeometry);
+
+    /**
+     * Stores the provided instance into the provided array.
+     * @param value The value to pack.
+     * @param array The array to pack into.
+     * @return The array that was packed into
+     */
+    @JsMethod
+    public static native double[] pack(RectangleGeometry value, double[] array);
+
+    /**
+     * Stores the provided instance into the provided array.
+     * @param value The value to pack.
+     * @param array The array to pack into.
+     * @param startingIndex The index into the array at which to start packing the elements.
+     * @return The array that was packed into
+     */
+    @JsMethod
+    public static native double[] pack(RectangleGeometry value, double[] array, Integer startingIndex);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @return The modified result parameter or a new RectangleGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native RectangleGeometry unpack(double[] array);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @param startingIndex The starting index of the element to be unpacked.
+     * @return The modified result parameter or a new RectangleGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native RectangleGeometry unpack(double[] array, Integer startingIndex);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @param startingIndex The starting index of the element to be unpacked.
+     * @param result The object into which to store the result.
+     * @return The modified result parameter or a new RectangleGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native RectangleGeometry unpack(double[] array, Integer startingIndex, RectangleGeometry result);
+}
---
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/core/geometry/options/RectangleGeometryOptions.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2016 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.core.geometry.options;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import org.cesiumjs.cs.core.Ellipsoid;
+import org.cesiumjs.cs.core.Rectangle;
+import org.cesiumjs.cs.core.VertexFormat;
+
+/**
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+public class RectangleGeometryOptions {
+    /**
+     * A cartographic rectangle with north, south, east and west properties in radians.
+     */
+    @JsProperty
+    public Rectangle rectangle;
+    /**
+     * The vertex attributes to be computed.
+     * Default {@link org.cesiumjs.cs.core.VertexFormat#DEFAULT()}
+     */
+    @JsProperty
+    public Integer vertexFormat;
+    /**
+     * The ellipsoid on which the rectangle lies.
+     * Default: {@link Ellipsoid#WGS84()}
+     */
+    @JsProperty
+    public Ellipsoid ellipsoid;
+    /**
+     * The distance, in radians, between each latitude and longitude. Determines the number of positions in the buffer.
+     * Default: {@link org.cesiumjs.cs.core.Math#RADIANS_PER_DEGREE()}
+     */
+    @JsProperty
+    public double granularity;
+    /**
+     * The distance in meters between the rectangle and the ellipsoid surface.
+     * Default: 0.0
+     */
+    @JsProperty
+    public double height;
+    /**
+     * The rotation of the rectangle, in radians. A positive rotation is counter-clockwise.
+     * Default: 0.0
+     */
+    @JsProperty
+    public double rotation;
+    /**
+     * The rotation of the texture coordinates, in radians. A positive rotation is counter-clockwise.
+     * Default: 0.0
+     */
+    @JsProperty
+    public double stRotation;
+    /**
+     * The distance in meters between the rectangle's extruded face and the ellipsoid surface.
+     */
+    @JsProperty
+    public double extrudedHeight;
+    /**
+     * Specifies whether the rectangle has a top cover when extruded.
+     * Default: true
+     */
+    @JsProperty
+    public boolean closeTop;
+    /**
+     * Specifies whether the rectangle has a bottom cover when extruded.
+     * Default: true
+     */
+    @JsProperty
+    public boolean closeBottom;
+
+    /**
+     * Options for {@link org.cesiumjs.cs.core.geometry.RectangleGeometry}
+     */
+    @JsConstructor
+    public RectangleGeometryOptions() {}
+}
