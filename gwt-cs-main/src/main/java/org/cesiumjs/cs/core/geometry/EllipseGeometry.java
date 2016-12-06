efd83cfbebd824e93aa869074995db7016d9dd4a
iserge <s.serge.b@gmail.com>
2016-12-06 10:29:26 +0300

    New Geometry - EllipseGeometry

---
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/core/geometry/EllipseGeometry.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2016 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.core.geometry;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import org.cesiumjs.cs.core.geometry.options.EllipseGeometryOptions;
+import org.cesiumjs.cs.scene.Primitive;
+
+/**
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = "Cesium", name = "EllipseGeometry")
+public class EllipseGeometry {
+    /**
+     * The number of elements used to pack the object into an array.
+     */
+    @JsProperty
+    public static double packedLength;
+
+    /**
+     * A description of an ellipse on an ellipsoid. Ellipse geometry can be rendered with both {@link Primitive} and {@link GroundPrimitive}.
+     */
+    @JsConstructor
+    public EllipseGeometry() {}
+
+    /**
+     * A description of an ellipse on an ellipsoid. Ellipse geometry can be rendered with both {@link Primitive} and {@link GroundPrimitive}.
+     * @param options {@link EllipseGeometryOptions}
+     */
+    @JsConstructor
+    public EllipseGeometry(EllipseGeometryOptions options) {}
+
+    /**
+     * Computes the geometric representation of a ellipse on an ellipsoid, including its vertices, indices, and a bounding sphere.
+     * @param ellipseGeometry A description of the ellipse.
+     * @return The computed vertices and indices.
+     */
+    @JsMethod
+    public static native Geometry createGeometry(EllipseGeometry ellipseGeometry);
+
+    /**
+     * Stores the provided instance into the provided array.
+     * @param value The value to pack.
+     * @param array The array to pack into.
+     * @return The array that was packed into
+     */
+    public static native double[] pack(EllipseGeometry value, double[] array);
+
+    /**
+     * Stores the provided instance into the provided array.
+     * @param value The value to pack.
+     * @param array The array to pack into.
+     * @param startingIndex The index into the array at which to start packing the elements.
+     * @return The array that was packed into
+     */
+    @JsMethod
+    public static native double[] pack(EllipseGeometry value, double[] array, int startingIndex);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @return The modified result parameter or a new EllipseGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native EllipseGeometry unpack(double[] array);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @param startingIndex The starting index of the element to be unpacked.
+     * @return The modified result parameter or a new EllipseGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native EllipseGeometry unpack(double[] array, int startingIndex);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @param startingIndex The starting index of the element to be unpacked.
+     * @param result The object into which to store the result.
+     * @return The modified result parameter or a new EllipseGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native EllipseGeometry unpack(double[] array, int startingIndex, EllipseGeometry result);
+}
---
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/core/geometry/options/EllipseGeometryOptions.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2016 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.core.geometry.options;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import org.cesiumjs.cs.core.Cartesian3;
+import org.cesiumjs.cs.core.Ellipsoid;
+import org.cesiumjs.cs.core.Math;
+import org.cesiumjs.cs.core.VertexFormat;
+
+/**
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+public class EllipseGeometryOptions {
+    /**
+     * The ellipse's center point in the fixed frame.
+     */
+    @JsProperty
+    public Cartesian3 center;
+    /**
+     * The length of the ellipse's semi-major axis in meters.
+     */
+    @JsProperty
+    public double semiMajorAxis;
+    /**
+     * The length of the ellipse's semi-minor axis in meters.
+     */
+    @JsProperty
+    public double semiMinorAxis;
+    /**
+     * The ellipsoid the ellipse will be on.
+     * Default: {@link Ellipsoid#WGS84()}
+     */
+    @JsProperty
+    public Ellipsoid ellipsoid;
+    /**
+     * The distance in meters between the ellipse and the ellipsoid surface.
+     * Default: 0.0
+     */
+    @JsProperty
+    public double height;
+    /**
+     * The distance in meters between the ellipse's extruded face and the ellipsoid surface.
+     */
+    @JsProperty
+    public double extrudedHeight;
+    /**
+     * The angle of rotation counter-clockwise from north.
+     * Default: 0.0
+     */
+    @JsProperty
+    public double rotation;
+    /**
+     * The rotation of the texture coordinates counter-clockwise from north.
+     * Default: 0.0
+     */
+    @JsProperty
+    public double stRotation;
+    /**
+     * The angular distance between points on the ellipse in radians.
+     * Default: {@link Math#RADIANS_PER_DEGREE()}
+     */
+    @JsProperty
+    public double granularity;
+    /**
+     * The vertex attributes to be computed.
+     * Default: {@link VertexFormat#DEFAULT()}
+     */
+    @JsProperty
+    public VertexFormat vertexFormat;
+
+    /**
+     * Options for {@link org.cesiumjs.cs.core.geometry.EllipseGeometry}
+     */
+    @JsConstructor
+    public EllipseGeometryOptions() {}
+}
