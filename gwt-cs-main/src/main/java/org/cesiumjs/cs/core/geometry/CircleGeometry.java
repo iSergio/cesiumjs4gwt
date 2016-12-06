db3d2b037ef894bac51382eb97ea3d3f8c66189a
iserge <s.serge.b@gmail.com>
2016-12-06 10:35:37 +0300

    New Geometry - CircleGeometry

---
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/core/geometry/CircleGeometry.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2016 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.core.geometry;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import org.cesiumjs.cs.core.geometry.options.CircleGeometryOptions;
+
+/**
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = "Cesium", name = "CircleGeometry")
+public class CircleGeometry {
+    /**
+     * The number of elements used to pack the object into an array.
+     */
+    @JsProperty
+    public static double packedLength;
+
+    @JsConstructor
+    public CircleGeometry(CircleGeometryOptions options) {}
+
+    /**
+     * Stores the provided instance into the provided array.
+     * @param value The value to pack.
+     * @param array The array to pack into.
+     * @return The array that was packed into
+     */
+    @JsMethod
+    public static native double[] pack(CircleGeometry value, double[] array);
+
+    /**
+     * Stores the provided instance into the provided array.
+     * @param value The value to pack.
+     * @param array The array to pack into.
+     * @param startingIndex The index into the array at which to start packing the elements.
+     * @return The array that was packed into
+     */
+    @JsMethod
+    public static native double[] pack(CircleGeometry value, double[] array, int startingIndex);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @return The modified result parameter or a new CircleGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native CircleGeometry unpack(double[] array);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @param startingIndex The starting index of the element to be unpacked.
+     * @return The modified result parameter or a new CircleGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native CircleGeometry unpack(double[] array, int startingIndex);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @param startingIndex The starting index of the element to be unpacked.
+     * @param result The object into which to store the result.
+     * @return The modified result parameter or a new CircleGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native CircleGeometry unpack(double[] array, int startingIndex, CircleGeometry result);
+}
