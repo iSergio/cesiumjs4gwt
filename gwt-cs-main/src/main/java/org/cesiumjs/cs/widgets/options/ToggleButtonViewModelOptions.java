7819be062a83dec86d231a6ae4a069b5a15624b7
iserge <s.serge.b@gmail.com>
2017-02-01 22:26:52 +0300

    Animation widget.

---
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/widgets/options/ToggleButtonViewModelOptions.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.widgets.options;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+/**
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+public class ToggleButtonViewModelOptions {
+    /**
+     * A boolean indicating whether the button should be initially toggled.
+     * Default: false
+     */
+    @JsProperty
+    public boolean toggled;
+    /**
+     * A string containing the button's tooltip.
+     * Default: empty string
+     */
+    @JsProperty
+    public String tooltip;
+
+    @JsConstructor
+    public ToggleButtonViewModelOptions() {}
+}
