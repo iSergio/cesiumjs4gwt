7d5ea4b2b19ddb3d4675ddd6c5f66d11c50cf3ea
iserge <s.serge.b@gmail.com>
2017-10-09 21:26:58 +0300

    Add CorridorGeometry support.

---
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/core/geometry/CorridorGeometry.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2017 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.core.geometry;
+
+import jsinterop.annotations.*;
+import org.cesiumjs.cs.core.Packable;
+import org.cesiumjs.cs.core.geometry.options.CorridorGeometryOptions;
+
+/**
+ * A description of a corridor. Corridor geometry can be rendered with both {@link org.cesiumjs.cs.scene.Primitive} and {@link org.cesiumjs.cs.scene.GroundPrimitive}.
+ *
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "CorridorGeometry")
+public class CorridorGeometry extends Geometry implements Packable {
+    /**
+     * The number of elements used to pack the object into an array.
+     */
+    @JsProperty
+    public static double packedLength;
+
+    /**
+     * A description of a corridor. Corridor geometry can be rendered with both {@link org.cesiumjs.cs.scene.Primitive} and {@link org.cesiumjs.cs.scene.GroundPrimitive}.
+     * @param options {@link CorridorGeometryOptions}.
+     */
+    @JsConstructor
+    public CorridorGeometry(CorridorGeometryOptions options) {}
+
+    /**
+     * Computes the geometric representation of a corridor, including its vertices, indices, and a bounding sphere.
+     * @param corridorGeometry A description of the corridor.
+     * @return The computed vertices and indices.
+     */
+    @JsMethod
+    public static native Geometry createGeometry(CorridorGeometry corridorGeometry);
+
+    /**
+     * Stores the provided instance into the provided array.
+     * @param value The value to pack.
+     * @param array The array to pack into.
+     * @return The array that was packed into
+     */
+    @JsMethod
+    public static native double[] pack(CorridorGeometry value, double[] array);
+
+    /**
+     * Stores the provided instance into the provided array.
+     * @param value The value to pack.
+     * @param array The array to pack into.
+     * @param startingIndex The index into the array at which to start packing the elements.
+     * @return The array that was packed into
+     */
+    @JsMethod
+    public static native double[] pack(CorridorGeometry value, double[] array, int startingIndex);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @return The modified result parameter or a new CorridorGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native CorridorGeometry unpack(double[] array);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @param startingIndex The starting index of the element to be unpacked.
+     * @return The modified result parameter or a new CorridorGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native CorridorGeometry unpack(double[] array, int startingIndex);
+
+    /**
+     * Retrieves an instance from a packed array.
+     * @param array The packed array.
+     * @param startingIndex The starting index of the element to be unpacked.
+     * @param result The object into which to store the result.
+     * @return The modified result parameter or a new CorridorGeometry instance if one was not provided.
+     */
+    @JsMethod
+    public static native CorridorGeometry unpack(double[] array, int startingIndex, CorridorGeometry result);
+}
---
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/core/geometry/options/CorridorGeometryOptions.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.core.geometry.options;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import org.cesiumjs.cs.core.Cartesian3;
+import org.cesiumjs.cs.core.Ellipsoid;
+import org.cesiumjs.cs.core.Math;
+import org.cesiumjs.cs.core.VertexFormat;
+import org.cesiumjs.cs.core.enums.CornerType;
+
+/**
+ * Options for {@link org.cesiumjs.cs.core.geometry.CorridorGeometry}
+ *
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+public class CorridorGeometryOptions {
+    /**
+     * An array of positions that define the center of the corridor.
+     */
+    @JsProperty
+    public Cartesian3[] positions;
+    /**
+     * The distance between the edges of the corridor in meters.
+     */
+    @JsProperty
+    public double width;
+    /**
+     * The ellipsoid to be used as a reference.
+     * Default: {@link org.cesiumjs.cs.core.Ellipsoid#WGS84()}
+     */
+    @JsProperty
+    public Ellipsoid ellipsoid;
+    /**
+     * The distance, in radians, between each latitude and longitude. Determines the number of positions in the buffer.
+     * Default: {@link Math#RADIANS_PER_DEGREE()}
+     */
+    @JsProperty
+    public double granularity;
+    /**
+     * The distance in meters between the ellipsoid surface and the positions.
+     * Default: 0
+     */
+    @JsProperty
+    public double height;
+    /**
+     * The distance in meters between the ellipsoid surface and the extruded face.
+     */
+    @JsProperty
+    public double extrudedHeight;
+    /**
+     * The vertex attributes to be computed.
+     * Default: {@link VertexFormat#DEFAULT()}
+     */
+    @JsProperty
+    public VertexFormat vertexFormat;
+    /**
+     * Determines the style of the corners.
+     * Default: {@link CornerType#ROUNDED()}
+     */
+    @JsProperty
+    public Number cornerType;
+
+    /**
+     * Options for {@link org.cesiumjs.cs.core.geometry.CorridorGeometry}
+     */
+    @JsConstructor
+    public CorridorGeometryOptions() {}
+}
