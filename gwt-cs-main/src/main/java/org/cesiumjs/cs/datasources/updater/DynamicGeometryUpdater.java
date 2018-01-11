fc869dfdcd27b1fe9293b082acc54be3caa55800
iSergio <s.serge.b@gmail.com>
2018-01-11 15:34:06 +0300

    GeometryUpdater support

---
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/datasources/updater/DynamicGeometryUpdater.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.datasources.updater;
+
+import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsType;
+import org.cesiumjs.cs.core.JulianDate;
+
+/**
+ * Defines the interface for a dynamic geometry updater. A {@link DynamicGeometryUpdater} is responsible for handling
+ * visualization of a specific type of geometry that needs to be recomputed based on simulation time.
+ * This object is never used directly by client code, but is instead created by {@link GeometryUpdater} implementations
+ * which contain dynamic geometry. This type defines an interface and cannot be instantiated directly.
+ *
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = "Cesium", name = "DynamicGeometryUpdater")
+public interface DynamicGeometryUpdater {
+    /**
+     * Destroys and resources used by the object. Once an object is destroyed, it should not be used.
+     */
+    @JsMethod
+    void destroy();
+
+    /**
+     * Returns true if this object was destroyed; otherwise, false.
+     * @return True if this object was destroyed; otherwise, false.
+     */
+    @JsMethod
+    boolean isDestroyed();
+
+    /**
+     * Updates the geometry to the specified time.
+     * @param time The current time.
+     */
+    @JsMethod
+    void update(JulianDate time);
+}
---
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/datasources/updater/RectangleGeometryUpdater.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2018 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.datasources.updater;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import org.cesiumjs.cs.datasources.Entity;
+import org.cesiumjs.cs.datasources.properties.Property;
+import org.cesiumjs.cs.scene.Scene;
+
+/**
+ * A {@link GeometryUpdater} for rectangles. Clients do not normally create this class directly,
+ * but instead rely on {@link org.cesiumjs.cs.datasources.DataSourceDisplay}.
+ *
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = "Cesium", name = "RectangleGeometryUpdater")
+public class RectangleGeometryUpdater extends GeometryUpdater {
+    /**
+     * Gets a value indicating if the geometry should be drawn on terrain.
+     */
+    @JsProperty(name = "onTerrain")
+    public native boolean onTerrain();
+    /**
+     * Gets the property specifying whether the geometry casts or receives shadows from each light source.
+     */
+    @JsProperty(name = "shadowsProperty")
+    public native Property shadowsProperty();
+
+    /**
+     * A {@link GeometryUpdater} for rectangles. Clients do not normally create this class directly,
+     * but instead rely on {@link org.cesiumjs.cs.datasources.DataSourceDisplay}.
+     * @param entity The entity containing the geometry to be visualized.
+     * @param scene The scene where visualization is taking place.
+     */
+    @JsConstructor
+    public RectangleGeometryUpdater(Entity entity, Scene scene) {
+        super(entity, scene);
+    }
+}
