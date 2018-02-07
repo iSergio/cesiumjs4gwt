9b7c5fe24bc5057ab9f7d39121b1588033ae6231
iSergio <s.serge.b@gmail.com>
2018-02-07 12:41:54 +0300

    Wrong official Cesium documentation

--- gwt-cs-main/src/main/java/org/cesiumjs/cs/core/Plane.java
+++ gwt-cs-main/src/main/java/org/cesiumjs/cs/core/Plane.java
@@ -163,18 +163,20 @@ public class Plane {
      * Projects a point onto the plane.
      * @param plane The plane to project the point onto
      * @param point The point to project onto the plane
+     * @result The result point.
      */
     @JsMethod
-    public static native void projectPointOntoPlane(Plane plane, Cartesian3 point);
+    public static native Cartesian3 projectPointOntoPlane(Plane plane, Cartesian3 point);
 
     /**
      * Projects a point onto the plane.
      * @param plane The plane to project the point onto
      * @param point The point to project onto the plane
      * @param result The result point. If undefined, a new Cartesian3 will be created.
+     * @return The result point.
      */
     @JsMethod
-    public static native void projectPointOntoPlane(Plane plane, Cartesian3 point, Cartesian3 result);
+    public static native Cartesian3 projectPointOntoPlane(Plane plane, Cartesian3 point, Cartesian3 result);
 
     /**
      * Transforms the plane by the given transformation matrix.
