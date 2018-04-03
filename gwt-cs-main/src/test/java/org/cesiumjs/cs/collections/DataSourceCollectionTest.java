441865f86b34fdb821ccafa5a8c78d55609a461e
iSergio <s.serge.b@gmail.com>
2018-04-03 16:29:40 +0300

    DataSourceCollection tests

---
+++ gwt-cs-main/src/test/java/org/cesiumjs/cs/collections/DataSourceCollectionTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.collections;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import org.cesiumjs.cs.BaseTestCase;
+import org.cesiumjs.cs.Cesium;
+import org.cesiumjs.cs.core.IonResource;
+import org.cesiumjs.cs.core.Request;
+import org.cesiumjs.cs.core.Resource;
+import org.cesiumjs.cs.core.options.ResourceOptions;
+import org.cesiumjs.cs.datasources.DataSource;
+import org.cesiumjs.cs.datasources.GeoJsonDataSource;
+import org.cesiumjs.cs.js.JsObject;
+import org.cesiumjs.cs.promise.Fulfill;
+import org.cesiumjs.cs.promise.Reject;
+import org.junit.Assert;
+
+/**
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+public class DataSourceCollectionTest extends BaseTestCase {
+    private DataSourceCollection collection;
+    private DataSource geojsonDS;
+    private DataSource topojsonDS;
+
+    public void testLower() {
+        delayTestFinish(10_000);
+
+        super.beginTest(new Test() {
+            @Override
+            public void execute() {
+                GeoJsonDataSource.load(GWT.getModuleBaseURL() + "SampleData/simplestyles.geojson").then(new Fulfill<GeoJsonDataSource>() {
+                    @Override
+                    public void onFulfilled(GeoJsonDataSource dataSource) {
+                        assertNotNull(dataSource);
+                        finishTest();
+                    }
+                }, new Reject<String>() {
+                    @Override
+                    public void onRejected(String value) {
+                        Cesium.log("onRejected: " + value);
+                        finishTest();
+                    }
+                });
+            }
+        });
+    }
+}
