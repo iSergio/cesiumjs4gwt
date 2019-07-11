635973b4494c28267a3dd66ef38b39cd8bdb5ae5
iSergio <s.serge.b@gmail.com>
2019-07-11 20:58:44 +0300

    ArcGISTiledElevationTerrainProvider support

---
+++ cesiumjs4gwt-main/src/main/java/org/cesiumjs/cs/core/providers/ArcGISTiledElevationTerrainProvider.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.core.providers;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsType;
+import org.cesiumjs.cs.core.Request;
+import org.cesiumjs.cs.core.TerrainData;
+import org.cesiumjs.cs.core.providers.options.ArcGISTiledElevationTerrainProviderOptions;
+import org.cesiumjs.cs.promise.Promise;
+
+/**
+ * A {@link TerrainProvider} that produces terrain geometry by tessellating height maps retrieved from Elevation Tiles
+ * of an an ArcGIS ImageService.
+ *
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = "Cesium", name = "ArcGISTiledElevationTerrainProvider")
+public class ArcGISTiledElevationTerrainProvider implements TerrainProvider {
+    @JsConstructor
+    private ArcGISTiledElevationTerrainProvider() {}
+
+    @JsConstructor
+    public ArcGISTiledElevationTerrainProvider(ArcGISTiledElevationTerrainProviderOptions options) {}
+
+    @Override
+    @JsMethod
+    public native double getLevelMaximumGeometricError(int level);
+
+    @Override
+    @JsMethod
+    public native boolean getTileDataAvailable(int x, int y, int level);
+
+    @Override
+    @JsMethod
+    public native Promise<TerrainData, Void> requestTileGeometry(int x, int y, int level, Request request);
+}
---
+++ cesiumjs4gwt-main/src/main/java/org/cesiumjs/cs/core/providers/options/ArcGISTiledElevationTerrainProviderOptions.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2019 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.core.providers.options;
+
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import org.cesiumjs.cs.core.Ellipsoid;
+import org.cesiumjs.cs.core.Resource;
+import org.cesiumjs.cs.promise.Promise;
+
+/**
+ * Options for {@link org.cesiumjs.cs.core.providers.ArcGISTiledElevationTerrainProvider}.
+ * s
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+public class ArcGISTiledElevationTerrainProviderOptions {
+    /**
+     * The URL of the ArcGIS ImageServer service.
+     */
+    @JsProperty
+    public Resource url;//	Resource | String | Promise.<Resource> | Promise.<String>
+
+    /**
+     * The URL of the ArcGIS ImageServer service.
+     */
+    @JsProperty(name = "url")
+    public String urlStr;
+    /**
+     * The URL of the ArcGIS ImageServer service.
+     */
+    @JsProperty(name = "url")
+    public Promise<Resource, Void> urlPromise;
+    /**
+     * The URL of the ArcGIS ImageServer service.
+     */
+    @JsProperty(name = "url")
+    public Promise<String, Void> urlPromiseSrt;
+    /**
+     * The authorization token to use to connect to the service.
+     */
+    @JsProperty
+    public String token;
+    /**
+     * The ellipsoid. If the tilingScheme is specified, this parameter is ignored and the tiling scheme's ellipsoid is used instead.
+     * If neither parameter is specified, the WGS84 ellipsoid is used.
+     */
+    @JsProperty
+    public Ellipsoid ellipsoid;
+
+    private ArcGISTiledElevationTerrainProviderOptions() {}
+
+    public static ArcGISTiledElevationTerrainProviderOptions create(Resource url) {
+        ArcGISTiledElevationTerrainProviderOptions options = new ArcGISTiledElevationTerrainProviderOptions();
+        options.url = url;
+        return options;
+    }
+
+    public static ArcGISTiledElevationTerrainProviderOptions create(String url) {
+        ArcGISTiledElevationTerrainProviderOptions options = new ArcGISTiledElevationTerrainProviderOptions();
+        options.urlStr = url;
+        return options;
+    }
+
+    public static ArcGISTiledElevationTerrainProviderOptions create(Promise<Resource, Void> promise) {
+        ArcGISTiledElevationTerrainProviderOptions options = new ArcGISTiledElevationTerrainProviderOptions();
+        options.urlPromise = promise;
+        return options;
+    }
+}
