b5471f03fe70751bd90a0a764b751bbe61efcd7c
iserge <s.serge.b@gmail.com>
2019-12-17 16:24:31 +0300

    Add

---
+++ cesiumjs4gwt-main/src/main/java/org/cesiumjs/cs/scene/providers/OpenStreetMapImageryProvider.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019 iserge.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cesiumjs.cs.scene.providers;
+
+/**
+ * An imagery provider that provides tiled imagery hosted by OpenStreetMap or another provider of Slippy tiles.
+ * The default url connects to OpenStreetMap's volunteer-run servers, so you must conform to their Tile Usage Policy.
+ *
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+public class OpenStreetMapImageryProvider implements ImageryProvider {
+
+}
